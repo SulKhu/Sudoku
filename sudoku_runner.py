0c55b1a1f05aa04c19d23ce5e47a0cbe338b1f26
SulKhu <sul.khu@gmail.com>
2024-08-16 14:15:55 -0700

    added run_game function that allows user to input their solution and parse the solution into a matrix

--- sudoku_runner.py
+++ sudoku_runner.py
@@ -4,15 +4,19 @@ from sudoku_generator import SudokuGenerator
 class SudokuRunner:
 
     def __init__(self):
-        self.possible_sizes = [3, 4, 9, 16]
-
-        self.possible_chars = [ 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                               'A', 'B', 'C', 'D', 'E', 'F',
-                               'G', 'H']
+        self.possible_sizes = [4, 9, 16]
+        self.size = 9
+        self.possible_chars = [ '1', '2', '3', '4',
+                                '5', '6', '7', '8',
+                                '9', 'A', 'B', 'C',
+                                'D', 'E', 'F', 'G', 'H' ]
     
-    def run_game(self):
-        print("Size of the puzzle is the length of one row. Default size is 9.")
-        print("Possible sizes: 3, 4, 9, 16")
+    def start_game(self):
+        print("Lets play Sudoku!")
+        print(" ")
+        print("Choose the size of your puzzle. The size of the puzzle is the length of one row. Default size is 9.")
+        print("Possible sizes: 4, 9, 16")
+        print(" ")
         char_size = input("Enter the desired size of the puzzle: ")
 
         try:
@@ -24,9 +28,12 @@ class SudokuRunner:
         if char_size not in self.possible_sizes:
             char_size = 9
 
+        self.size = char_size
+        print(" ")
         print("Puzzle size will be " + str(char_size))
 
-        print("Difficulty of the puzzle ranges from 1-5, where 1 is the easiest and 5 is the most difficult. Default difficulty is 3")
+        print(" ")
+        print("The difficulty of the puzzle ranges from 1-5, where 1 is the easiest and 5 is the most difficult. Default difficulty is 3")
         difficulty = input("Enter the desired difficulty of the puzzle: ")
 
         try:
@@ -41,6 +48,7 @@ class SudokuRunner:
         if difficulty > 5:
             difficulty = 5
 
+        print(" ")
         print("Puzzle difficulty will be " + str(difficulty))
 
         chars = self.possible_chars[0: char_size]
@@ -50,10 +58,44 @@ class SudokuRunner:
         puzzles = generator.generate_puzzle()
 
         unsolved_puzzle = puzzles[0]
-        solution = puzzles[1]
 
         generator.print_puzzle(unsolved_puzzle)
 
+        return puzzles
+
+    def run_game(self):
+        puzzles = self.start_game()
+        print("To check answer, copy paste the puzzle into a app like notebook and paste in the completed or partially completed puzzle")
+        print("To see solution, type 'key'")
+
+        print("Enter the your solution here: ")
+
+        player_solution = []
+
+        for i in range((self.size * 3) + 1):
+            curr_line = input()
+            if curr_line[0] == "_" or curr_line[0] == " ":
+                continue
+
+            proper_vals = []
+            curr_index = 0
+            for char in curr_line:
+                
+                try:
+                    if curr_line[curr_index: curr_index + 3] == '   ':
+                        proper_vals.append(None)
+
+                    elif char in self.possible_chars:
+                        proper_vals += char
+
+                except:
+                    if char in self.possible_chars:
+                        proper_vals += char
+                
+                curr_index += 1
+            player_solution.append(proper_vals)
+
+        print(player_solution)
 
 r = SudokuRunner()
 r.run_game()
